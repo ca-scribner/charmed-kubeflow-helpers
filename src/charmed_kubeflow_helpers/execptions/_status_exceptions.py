4e77a475c07f94517b2a78df9ff4234c3f6042ff
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-02 09:48:55 -0400

    Add exceptions, basic structure and package files

---
+++ src/charmed_kubeflow_helpers/execptions/_status_exceptions.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+from ops.model import BlockedStatus, WaitingStatus, ActiveStatus
+
+
+class ErrorWithStatus(Exception):
+    """Raised when an exception occurs and the raiser has an opinion on the resultant charm status
+    """
+    # TODO: Should this status base class just accept an instanced Status rather than msg and status_type?
+    #       The msg+type feels like a chore for the user
+    def __init__(self, msg: str, status_type: Union[ActiveStatus, WaitingStatus, BlockedStatus, None]):
+        super().__init__(str(msg))
+        self.msg = str(msg)
+        self.status_type = status_type
+
+    @property
+    def status(self):
+        return self.status_type(self.msg)
+
+
+class LeadershipError(ErrorWithStatus):
+    """Raised when a charm should be in WaitingStatus because it is not the leader"""
+    def __init__(
+            self,
+            msg: str = "Waiting for leadership",
+            status_type: Union[ActiveStatus, WaitingStatus, BlockedStatus, None] = WaitingStatus
+    ):
+        super().__init__(msg, status_type)
