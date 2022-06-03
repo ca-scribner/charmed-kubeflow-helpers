faadde9d7b6032812ec2b5b436bc926ae9b14ef8
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-03 11:46:20 -0400

    fix CharmStatusType class type hint

--- src/charmed_kubeflow_helpers/status_handling/_charm_status_type.py
+++ src/charmed_kubeflow_helpers/status_handling/_charm_status_type.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import Type, Union
 from ops.model import ActiveStatus, WaitingStatus, BlockedStatus, MaintenanceStatus
 
-CharmStatusType = Union[ActiveStatus, WaitingStatus, BlockedStatus, MaintenanceStatus]
+# Convenience type for accepting one of the Status classes (not an instance of the class)
+CharmStatusType = Type[Union[ActiveStatus, WaitingStatus, BlockedStatus, MaintenanceStatus]]
